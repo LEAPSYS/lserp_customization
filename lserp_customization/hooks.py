297bde21021ce1e2a9acbf2e188919e2dfc8e124
Satish Tripathi <stripathi@leapsys.net>
2026-03-13 16:24:15 +0530

    fix(build): rename js to .bundle.js and update hooks for frappe esbuild discovery

--- lserp_customization/hooks.py
+++ lserp_customization/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/lserp_customization/css/lserp_customization.css"
-app_include_js = "/assets/lserp_customization/js/lserp_theme_injector.js"
+app_include_js = "/assets/lserp_customization/js/lserp_theme_injector.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/lserp_customization/css/lserp_customization.css"
